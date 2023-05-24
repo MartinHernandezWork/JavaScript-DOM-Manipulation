728110376357a86b450acaaa4002cdef3c05512d
Martin Hernandez <martinhernandezunvime@gmail.com>
2023-05-24 11:13:47 -0300

    Update 24/5

---
+++ Selectiong-Elements-in-the-DOM/app.js
@@ -0,0 +1,33 @@
+// DOM Manipulation
+
+
+
+//GetElementById()
+
+const tittle = document.getElementById('main-heading');
+
+console.log(tittle);
+
+//GetElementByClassName()
+
+const listItem = document.getElementsByClassName('list-items');
+
+console.log(listItem);
+
+//getElementByTagName()
+
+const li = document.getElementsByTagName('li');
+
+console.log(li);
+
+//quearySelector()
+
+const first = document.querySelector('div');
+
+console.log(first);
+
+//querySelectorAll()
+
+const all = document.querySelectorAll('div')
+
+console.log(all)
