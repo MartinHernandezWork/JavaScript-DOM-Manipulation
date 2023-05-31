9e619c9c6f64108e987b15305568220ed094a2e4
Martin Hernandez <martinhernandezunvime@gmail.com>
2023-05-31 12:10:18 -0300

    update 31/5

--- Selectiong-Elements-in-the-DOM/app.js
+++ Selectiong-Elements-in-the-DOM/app.js
@@ -6,28 +6,18 @@
 
 const tittle = document.getElementById('main-heading');
 
-console.log(tittle);
-
 //GetElementByClassName()
 
 const listItem = document.getElementsByClassName('list-items');
-
-console.log(listItem);
 
 //getElementByTagName()
 
 const li = document.getElementsByTagName('li');
 
-console.log(li);
-
 //quearySelector()
 
 const first = document.querySelector('div');
 
-console.log(first);
-
 //querySelectorAll()
 
 const all = document.querySelectorAll('div')
-
-console.log(all)
