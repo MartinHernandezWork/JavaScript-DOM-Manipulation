c2d55a13d4cdfdaeff41b4be7a07d2ad8c182998
Martin Hernandez <martinhernandezunvime@gmail.com>
2023-05-31 13:06:31 -0300

    Update 31/5

--- Adding-elements/script.js
+++ Adding-elements/script.js
@@ -1,13 +1,18 @@
 // Adding Elements (with append)
 
-/*Creamos una variable y le asignamos a donde queremos apuntar con
+/* Creamos una variable y le asignamos a donde queremos apuntar con
 document. en este caso al body de nuestro HTML */
+
+/* Luego incorporamos nuestros elementos con .append() */
 
 const body = document.body;
 
 body.append("Hello world ", "Bye");
 
 // Creating Elements
+
+/* Creamos un elemento con document.createElement('') puede ser cualquier
+tipo */
 
 const div = document.createElement('div');
 
