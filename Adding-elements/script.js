f55fe7f53ce563ed76dc7eff8885dba9e0507f57
Martin Hernandez <86983543+MartinHernandezWork@users.noreply.github.com>
2023-06-01 12:53:25 -0300

    update 1/6

--- Adding-elements/script.js
+++ Adding-elements/script.js
@@ -17,3 +17,26 @@ tipo */
 const div = document.createElement('div');
 
 body.append(div);
+
+//Modifying Element Text
+
+/* agregamos texto a nuestro div .
+
+NOTA: existen 2 maneras de declara texto, innerText y textContent, lo que cambia es la visibilidad
+y la forma en que se interpreta que se quiere mostrar y que no. */
+
+div.innerText = "Hello web!"
+
+//Modifying Element HTML
+
+/* para usar html en js tenemos que utilizar innerHTML */
+
+div.innerHTML = "<h2>Bye web!</h2>"
+
+/* NOTA: Esto es una manera poco segura de integrar HTML en js, ya que nuestros usuarios podrian escribir
+codigo malicioso. para esto, existe otra manera de replicar lo anterior de una manera mas segura,
+de la siguiente manera: */
+
+const strong = document.createElement('strong')
+strong.innerText = "Hello world 2"
+div.append(strong)
---
+++ Removing-elements/app.js
@@ -0,0 +1,11 @@
+const body = document.body;
+
+const div = document.querySelector("div");
+const element = document.getElementById("arg");
+const elementTwo = document.getElementById("br");
+
+// Removing a element
+
+/* para remover un elemento utilizamos .remove() */
+
+element.remove();
